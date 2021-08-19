ff36b26b9d95d160c6f571824d88e3d57f38f820
Александр Грачев <A.grachev94@yandex.ru>
2021-08-19 10:42:08 +0300

    подготовил шаблон проекта

---
+++ src/test/java/ru/tinkoff/tests/TestBase.java
@@ -0,0 +1,39 @@
+package ru.tinkoff.tests;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import ru.tinkoff.config.Project;
+import ru.tinkoff.helpers.AllureAttachments;
+import ru.tinkoff.helpers.DriverSettings;
+import ru.tinkoff.helpers.DriverUtils;
+
+
+@ExtendWith({AllureJunit5.class})
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DriverSettings.configure();
+    }
+
+    @AfterEach
+    public void addAttachments() {
+        String sessionId = DriverUtils.getSessionId();
+
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        AllureAttachments.addPageSource();
+//        AllureAttachments.attachNetwork(); // todo
+        AllureAttachments.addBrowserConsoleLogs();
+
+        Selenide.closeWebDriver();
+
+        if (Project.isVideoOn()) {
+            AllureAttachments.addVideo(sessionId);
+        }
+    }
+}
